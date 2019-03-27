003491d07cc99034765cf4c3d06addec5394af3f
fruizga <ruizgallego@gmail.com>
2019-03-27 17:25:30 -0500

    Primer commit

---
+++ datos.js
@@ -0,0 +1,30 @@
+const opciones={
+	nombre:{
+		alias:'n',
+		demand:true
+	},
+	matematicas:{
+		demand:true,
+		alias:'m'
+	},
+	ingles:{
+		demand:true,
+		alias:'i'
+	},
+	programacion:{
+		demand:true,
+		alias:'p'
+	}
+
+}
+
+let obtenerPromedio=(nota_uno,nota_dos,nota_tres)=>console.log('el promedio 3 es '+(nota_uno+nota_dos+nota_tres)/3);
+
+const argv= require('yargs')
+	.command('promedio', 'Calcular el promedio', opciones)
+	.argv
+
+module.exports = {
+	obtenerPromedio,
+	argv
+};
